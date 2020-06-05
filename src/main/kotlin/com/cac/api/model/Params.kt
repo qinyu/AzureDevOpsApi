a31299f536710c554a0f9c121c9032b8a0b0b45b
junbin.huang@thoughtworks.com <junbin.huang@thoughtworks.com>
2020-06-05 10:08:06 +0800

    change the default project name

--- src/main/kotlin/com/cac/api/model/Params.kt
+++ src/main/kotlin/com/cac/api/model/Params.kt
@@ -2,7 +2,7 @@ package com.cac.api.model
 
 
 data class Params(var organization: String = "cac-technical",
-                  var project: String = "BaseProject",
+                  var project: String = "TLTraining",
                   var version: String = "6.0-preview.6",
                   var repositoryId: String?,
                   var repositoryType: String?,
